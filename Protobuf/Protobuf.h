a3a532fca008a0de99b316509da2cba2a1fc85f7
Nghia Tran <vinhnghiatran@gmail.com>
2020-11-14 11:35:15 +0700

    Initial Commit

---
+++ Protobuf/Protobuf.h
@@ -0,0 +1,18 @@
+//
+//  Protobuf.h
+//  Protobuf
+//
+//  Created by Nghia Tran on 14/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Protobuf.
+FOUNDATION_EXPORT double ProtobufVersionNumber;
+
+//! Project version string for Protobuf.
+FOUNDATION_EXPORT const unsigned char ProtobufVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Protobuf/PublicHeader.h>
+
+
